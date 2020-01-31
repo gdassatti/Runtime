5f26093acdbc9a143a3a2392601ab659fa0d70b7
gdassatti <gd221717@ohio.ed>
2020-01-30 21:13:58 -0500

    final

--- main.cc
+++ main.cc
@@ -18,9 +18,7 @@ using namespace std;
 int menu();
 
 int main( ){
-    
 	Runtime tmp;
-
 // Test your input and output operators with this:
 /*
   	cout << "Enter a run: ";
@@ -29,144 +27,79 @@ int main( ){
     cout << tmp << endl;
     cout << "Good-bye.\n";
 */
-
    	Runjournal myruns; 
     ifstream ifs;
     ofstream ofs;
     int choice;
     double dist;
-	//string filename;
-/*
-	cout << endl;
-	cout << "Please enter the name of the file you would like to work with: ";
-	cin >> filename;
-*/
 
-  
 	ifs.open("runlog.txt");
     if(!ifs.fail()){
-
 		ifs >> tmp;
-
 		while(!ifs.eof()){
-
 	    	myruns.record(tmp);
 	   	 	ifs >> tmp;
-
 		}
-
 	ifs.close();
-
 	}
 
     do{
-		
-		choice = menu();
-		switch(choice){
-
-			case 1: 
-
-					cout << "Enter the run here with spaces in between: ";
-					cin >> tmp;
-					myruns.record(tmp);  
-					break;
-		
-			case 2:  
-
-					cout << "Enter the time and distance of the run here: ";
-					cin >> tmp;
-					myruns.find_remove(tmp);  
-					break;
-		
-			case 3:	
-			
-					cout << endl;
-					myruns.display(); 
-					cout << endl;
-					break;
-		
-			case 4:	
-			
-					cout << endl;
-					cout << "Enter a distance to view: ";
-					cin >> dist;
-					myruns.distance_view(dist); 
-					cout << endl;
-					break;
-		
-			case 5:	
-			
-					myruns.time_sort(); 
-					cout << endl;
-					myruns.display();
-					cout << endl;
-					break;
-		
-			case 6:	
-			
-					myruns.distance_sort(); 
-					cout << endl;
-					myruns.display();
-					cout << endl;
-					break;
-		
-			case 7:  
-			
-					cout << endl;
-					cout << "Total time: " << myruns.total_time() << endl; 
-					cout << endl;
-					break;
-		
-			case 8: 
-			
-					cout << endl;
-					cout << "Total distance: " << myruns.total_distance() << " miles" << endl; 
-					cout << endl;
-					break;
-		
-			case 9: 
-			
-					cout << endl;
-					cout << "Average pace: " << myruns.average_pace() << " per mile." << endl; 
-					cout << endl;
-					break;
-		
-			case 0: 
-
-					cout << endl;
-					cout  << "Thank you for using our software.\n";
-					cout << endl;
-					break;
-			
-			default: 
-			
-					cout << endl;
-					cout << "Not a valid selection.\n";
-					break;
-
-			} // bottom of the switch
-
-    } while(choice != 0);
+	choice = menu();
+	switch(choice){
+		case 1: 
+			cin >> tmp;
+			myruns.record(tmp);  
+			break;
+		case 2:  
+			cin >> tmp;
+			myruns.find_remove(tmp);  
+			break;
+		case 3:		
+			myruns.display(); 
+			break;
+		case 4:		
+			cin >> dist;
+			myruns.distance_view(dist); 
+			break;
+		case 5:	
+			myruns.time_sort(); 
+			myruns.display();
+			break;
+		case 6:		
+			myruns.distance_sort(); 
+			myruns.display();
+			break;
+		case 7:  	
+			cout << myruns.total_time() << endl; 
+			break;
+		case 8: 	
+			cout << myruns.total_distance() << endl; 
+			break;		
+		case 9: 
+			cout << myruns.average_pace() << endl; 
+			break;
+		case 0: 
+			cout  << "Thank you for using our software.\n";
+			break;		
+		default: 
+			cout << "Not a valid selection.\n";
+			break;
+			} 
+    }while(choice != 0);
 
 //file saving
 
 	ofs.open("runlog.txt");
 	if(!ofs.fail()){
-
 	    myruns.save(ofs); 
 	    ofs.close();
-
  	}
-
 
 return 0;
 }
 
 int menu( ){
-
     int selection;
-
-	cout << endl;
     cout<<"Chose from the Following\n";
     cout<<"1.) RECORD a run\n";
     cout<<"2.) FIND a run by entering time and distance w option to remove\n";
@@ -178,10 +111,7 @@ int menu( ){
     cout<<"8.) TOTAL DISTANCE\n";
     cout<<"9.) AVERAGE PACE\n";
     cout<<"0.) QUIT\n";
-
 	cout << "Enter your choice here: ";
     cin >> selection;
-
     return selection;
-
 }
